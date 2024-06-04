198737e1a2020ca4b1e1dc74c6da7cf0610b3ecf
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-04 02:22:27 -0500

    completed sign-up route

--- server/auth.py
+++ server/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from . import db 
+from models import User
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
@@ -11,9 +15,27 @@ def login():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    username = request.form.get('username')
-    password1 = request.form.get('password1')
-    password2 = request.form.get('password2')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        username_exists = User.query.filter_by(username=username).first()
+
+        if username_exists:
+            flash('Username already in use', category='error')
+        elif password1 != password2:
+            flash('password don\'t match', category='error') 
+        elif len(username) < 2:
+            flash('Username is too short', category='error') 
+        elif len(password1)  < 6:
+            flash('password is too short', category='error')
+        else:
+            new_user = User(username=username, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('User created')
+            return redirect(url_for('views.home'))
     
     return render_template("signup.html")
 
