887b7c1dcef157797140d15d652c1b27dee1a249
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-04 02:32:08 -0500

    fixed secret bug

--- server/auth.py
+++ server/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from . import db 
-from models import User
-from flask_login import login_user, logout_user, login_required
+from .models import User
+from flask_login import login_user, logout_user, login_required, current_user 
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
@@ -54,5 +54,7 @@ def sign_up():
     return render_template("signup.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "logout"
+    logout_user()
+    return redirect(url_for('views.login'))
