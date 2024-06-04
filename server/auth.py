1015d01a3ca55959ceb350ac260aae0dbb838428
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-04 01:36:59 -0500

    added create database function

--- server/auth.py
+++ server/auth.py
@@ -1,13 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
     return render_template("login.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    username = request.form.get('username')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+    
     return render_template("signup.html")
 
 @auth.route('/logout')
