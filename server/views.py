5eb361a8c891237421a5d0db171aec511ac466fa
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-04 04:43:18 -0500

    home page now displays name and balance

--- server/views.py
+++ server/views.py
@@ -1,7 +1,17 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from .models import User
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
+    user = current_user
+    if user.is_authenticated:
+        user_details = User.query.get(user.id)
+        name = user_details.username 
+        balance = user_details.balance
+        return render_template('home.html', name=name, balance=balance)
+
     return render_template("home.html")
+
