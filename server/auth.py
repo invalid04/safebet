3316b0e5e9224a3d8183236a050a00878b6f62f6
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-04 02:26:40 -0500

    completed login route

--- server/auth.py
+++ server/auth.py
@@ -8,8 +8,21 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Password is incorrect', category='error')
+        else:
+            flash('Username does not exist')
 
     return render_template("login.html")
 
@@ -34,6 +47,7 @@ def sign_up():
             new_user = User(username=username, password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('User created')
             return redirect(url_for('views.home'))
     
