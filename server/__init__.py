d2dbeff33fae803248dd7a3e936f98ec2112cfc5
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-06-04 01:03:45 -0500

    created home route

--- server/__init__.py
+++ server/__init__.py
@@ -7,4 +7,8 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET-KEY'] = "xicfhnasmlefpo"
 
+    from .views import views 
+
+    app.register_blueprint(views, url_prefix='/')
+
     return app 
